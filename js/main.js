4071d7940153f4a0a8e5a8eae46f1b69a4cff1a1
Jake <forget_it_99@hotmail.co.uk>
2021-05-16 20:20:51 +0100

    Initial checkin

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    function init() {
+
+    }
+
+    $(document).ready(function() {
+        $('#brandImg').animate({ left: "25%", opacity: "100%" }, 1000);
+        $('#comingSoon').animate({ left: "50%", opacity: "100%" }, 1000);
+    });
+
+    init();
+})();
